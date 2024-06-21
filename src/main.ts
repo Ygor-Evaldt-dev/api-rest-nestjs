1b5de7e4054aa7d0389b787863169305f5d4bf11
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-06-21 06:56:14 -0300

    adicionado swagger com configurações básicas

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalPipes(
         new ValidationPipe({
             dismissDefaultMessages: true,
@@ -13,6 +15,16 @@ async function bootstrap() {
             transform: true,
         }),
     );
+
+    const config = new DocumentBuilder()
+        .setTitle('API REST - Lista de tarefas')
+        .setDescription('Construída para servir aplicações web onde um usuário pode descrever e salvar as tarefas que deseja realizar, além de poder sinalizar as tarefas que já concluiu')
+        .setVersion('1.0')
+        .addTag('Usuário')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
