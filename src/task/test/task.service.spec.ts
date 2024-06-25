b3b0575b4ba639fcbc69521134ebec43151037be
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-06-25 18:01:20 -0300

    garantido trazer todas as tarefas por usuario

--- src/task/test/task.service.spec.ts
+++ src/task/test/task.service.spec.ts
@@ -4,15 +4,19 @@ import { getTestingModule } from './util/get-testing-module';
 import { tasks } from './util/tasks';
 import { UserService } from 'src/user/user.service';
 import { users } from 'src/shared/test/users';
+import { User } from 'src/user/entities/user.entity';
 
 describe('TaskService', () => {
     let taskService: TaskService;
     let userService: UserService;
+    let user: User;
 
     beforeAll(async () => {
         const module: TestingModule = await getTestingModule();
         taskService = module.get<TaskService>(TaskService);
         userService = module.get<UserService>(UserService);
+
+        user = await userService.findByEmail(users.exists.email);
     });
 
     it('should be defined', () => {
@@ -22,19 +26,40 @@ describe('TaskService', () => {
 
     describe('create', () => {
         it('shoult create a new task', async () => {
-            const { id } = await userService.findByEmail(users.exists.email);
             const exec = async () => taskService.create({
                 ...tasks.new,
-                userId: id
+                userId: user.id
             });
 
             await expect(exec()).resolves.not.toThrow();
         });
     });
 
-    describe('find many', () => {
-        it('should', async () => {
+    describe('find all', () => {
+        it('should throw NotFoundException if there are not user tasks', async () => {
+            const exec = async () => taskService.findAll({
+                page: 0,
+                take: 10,
+                userId: 0
+            });
 
+            await expect(exec()).rejects.toThrow('Nenhuma tarefa cadastrada');
         });
-    })
+
+        it('should return tasks by user', async () => {
+            const page = 0;
+            const take = 10;
+            const response = await taskService.findAll({
+                page,
+                take,
+                userId: user.id
+            });
+
+            expect(response.total).toBeGreaterThan(0);
+            expect(response.registers.length).toBeGreaterThan(0);
+            expect(response.page).toBe(page);
+            expect(response.take).toBe(take);
+        });
+    });
+
 });
